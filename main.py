2813514bf85fd7c120c9cee968b3b769f9882de3
als1379 <a.saleh.m.a@gmail.com>
2020-03-11 00:38:13 +0330

    call director page crawler

--- main.py
+++ main.py
@@ -1,18 +1,27 @@
-from Movie import Movie, Actor, Director
-from crawler import director_page_crawler
-
-
-def pretty(d, indent=0):
-    for key, value in d.items():
-        print('\t' * indent + str(key))
-        if isinstance(value, dict):
-            pretty(value, indent+1)
-        else:
-            print('\t' * (indent+1) + str(value))
-
-
+from Movie import Movie, Director
+from crawler import Crawler
 try:
-    result = director_page_crawler("https://www.imdb.com/name/nm0634240/")
-    pretty(result)
+    print("hello")
+    crawler = Crawler("https://www.imdb.com/name/nm0000233/")
+    result = crawler.director_page_crawler()
+    director = Director(result['name'], result['photo'], result['age'], result['nation'])
+    print(director)
+    for i in result['movies']:
+        try:
+            url = "https://www.imdb.com" + i['url']
+            crawler.url = url
+            m_result = crawler.movie_page_crawler()
+            if m_result == {}:
+                continue
+            movie = Movie(m_result['title'], m_result['year'],
+                          m_result['genres'], m_result['rate'],
+                          m_result['rates_num'], m_result['actors'])
+            director.add_movie(movie)
+            print(movie)
+            print(movie.actors)
+        except Exception as e:
+            print(str(e))
+    director.show_movies()
 except Exception as e:
     print(str(e))
+

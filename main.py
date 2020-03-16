8d523c4033e514a82516f60b55261a9e7d8e1053
als1379 <a.saleh.m.a@gmail.com>
2020-03-16 23:37:23 +0330

    print actors

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 from Movie import Movie, Director
-from crawler import Crawler
+from crawler import *
 try:
-    print("hello")
-    crawler = Crawler("https://www.imdb.com/name/nm0000233/")
-    result = crawler.director_page_crawler()
+    result = director_page_crawler("https://www.imdb.com/name/nm3227090/")
     director = Director(result['name'], result['photo'], result['age'], result['nation'])
     print(director)
     for i in result['movies']:
         try:
             url = "https://www.imdb.com" + i['url']
-            crawler.url = url
-            m_result = crawler.movie_page_crawler()
+            m_result = movie_page_crawler(url)
             if m_result == {}:
                 continue
             movie = Movie(m_result['title'], m_result['year'],
@@ -21,7 +18,8 @@ try:
             print(movie.actors)
         except Exception as e:
             print(str(e))
-    director.show_movies()
+    print(director.show_movies())
+    print(director.movies[1].actors)
 except Exception as e:
     print(str(e))
 

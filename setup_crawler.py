09aea7d2505daba63c9daaca1efbcceb541fbd9d
als1379 <a.saleh.m.a@gmail.com>
2020-03-18 03:20:50 +0330

    setup_crawler created

---
+++ setup_crawler.py
@@ -0,0 +1,57 @@
+from crawler import *
+from Movie import Actor, Movie, Director
+
+
+def actor_handler(actors_name):
+    for actor in Actor.actors:
+        if actor.name == actors_name:
+            return actor
+    actor = Actor(actors_name)
+    Actor.actors.append(actor)
+    return actor
+
+
+def movie_crawler_handler(url):
+    try:
+        movie_crawl_result = movie_page_crawler(url)
+        movie_flag = False
+        movie = None
+        for _movie in Movie.movies:
+            if _movie.title == movie_crawl_result['title']:
+                movie = _movie
+                movie_flag = True
+                break
+        if not movie_flag:
+            movie = Movie(movie_crawl_result['title'], movie_crawl_result['year'],
+                          movie_crawl_result['genres'], movie_crawl_result['rate'],
+                          movie_crawl_result['rates_num'])
+            for actor in movie_crawl_result['actors']:
+                movie.add_actor(actor_handler(actor))
+        print(movie)
+        print(movie.show_actors())
+        return movie
+    except Exception as e:
+        raise Exception('error in handler', str(e))
+
+
+def director_crawler_handler(url):
+    try:
+        director_crawl_result = director_page_crawler(url)
+        director_flag = False
+        director = None
+        for _director in Director.directors:
+            if _director.name == director_crawl_result['name']:
+                director = _director
+                director_flag = True
+                break
+        if not director_flag:
+            director = Director(director_crawl_result['name'], director_crawl_result['photo'],
+                                director_crawl_result['age'], director_crawl_result['nation'])
+            print(director)
+            for i in director_crawl_result['movies']:
+                movie_url = "https://www.imdb.com" + i['url']
+                director.add_movie(movie_crawler_handler(movie_url))
+            Director.directors.append(director)
+        return director
+    except Exception as e:
+        raise Exception('error in handler', str(e))

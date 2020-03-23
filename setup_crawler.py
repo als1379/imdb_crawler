d56559c4fe61ef3adf6832e40d960e654070f8cf
als1379 <a.saleh.m.a@gmail.com>
2020-03-23 15:12:16 +0430

    add to 250 crawler handler

--- setup_crawler.py
+++ setup_crawler.py
@@ -1,4 +1,4 @@
-from crawler import director_page_crawler, movie_page_crawler
+from crawler import director_page_crawler, movie_page_crawler, top_250_crawler
 from Movie import Actor, Movie, Director
 
 
@@ -14,19 +14,15 @@ def actor_handler(actors_name):
 def movie_crawler_handler(url):
     try:
         movie_crawl_result = movie_page_crawler(url)
-        movie_flag = False
-        movie = None
         for _movie in Movie.movies:
             if _movie.title == movie_crawl_result['title']:
-                movie = _movie
-                movie_flag = True
-                break
-        if not movie_flag:
-            movie = Movie(movie_crawl_result['title'], movie_crawl_result['year'],
-                          movie_crawl_result['genres'], movie_crawl_result['rate'],
-                          movie_crawl_result['rates_num'])
-            for actor in movie_crawl_result['actors']:
-                movie.add_actor(actor_handler(actor))
+                return _movie
+        movie = Movie(movie_crawl_result['title'], movie_crawl_result['year'],
+                      movie_crawl_result['genres'], movie_crawl_result['rate'],
+                      movie_crawl_result['rates_num'])
+        for actor in movie_crawl_result['actors']:
+            movie.add_actor(actor_handler(actor))
+        Movie.movies.append(movie)
         return movie
     except Exception as e:
         raise e
@@ -35,25 +31,32 @@ def movie_crawler_handler(url):
 def director_crawler_handler(url):
     try:
         director_crawl_result = director_page_crawler(url)
-        director_flag = False
-        director = None
         for _director in Director.directors:
             if _director.name == director_crawl_result['name']:
-                director = _director
-                director_flag = True
-                break
-        if not director_flag:
-            director = Director(director_crawl_result['name'], director_crawl_result['photo'],
-                                director_crawl_result['age'], director_crawl_result['nation'])
-            print(director)
-            for i in director_crawl_result['movies']:
-                movie_url = "https://www.imdb.com" + i['url']
-                try:
-                    director.add_movie(movie_crawler_handler(movie_url))
-                    print("crawl complete")
-                except Exception as e:
-                    print(str(e))
-            Director.directors.append(director)
+                return _director
+        director = Director(director_crawl_result['name'], director_crawl_result['photo'],
+                            director_crawl_result['age'], director_crawl_result['nation'])
+        print(director)
+        for i in director_crawl_result['movies']:
+            movie_url = "https://www.imdb.com" + i['url']
+            try:
+                director.add_movie(movie_crawler_handler(movie_url))
+                print("crawl complete")
+            except Exception as e:
+                print(str(e))
+        Director.directors.append(director)
         return director
+    except Exception as e:
+        print(str(e))
+
+
+def top_250_crawler_handler(url):
+    try:
+        director_links = top_250_crawler(url)
+        print(director_links)
+        for link in director_links:
+            print(link)
+            director = director_crawler_handler(link)
+            print(director, "complete")
     except Exception as e:
         print(str(e))

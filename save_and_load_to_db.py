e62cfe30b836c8451cf423779e3e7a1ef044e5d4
als1379 <a.saleh.m.a@gmail.com>
2020-04-01 01:35:44 +0430

    create save and load directors urls

---
+++ save_and_load_to_db.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+def save_directors_links(d_link):
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    i = 1
+    for link in d_link:
+        c.execute("INSERT INTO dLinks VALUES ('{}')".format(link))
+        print(i, link)
+        i += 1
+        conn.commit()
+    conn.close()
+
+
+def load_directors_links():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM dLinks")
+    links = []
+    for data in c.fetchall():
+        print(data[0])
+        links.append(data[0])
+    conn.close()
+
+    return links
